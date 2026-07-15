ddbc6f6838aaa8d0f44f0386f10a0fdc14979c4c
Yu Lia <1938793150@qq.com>
2026-07-15 15:46:51 +0800

    perf: optimize user experience

--- app/components/designer/properties/NodeProperty.vue
+++ app/components/designer/properties/NodeProperty.vue
@@ -12,10 +12,12 @@ const editorStore = useEditorStore()
 const { selectedNode } = storeToRefs(editorStore)
 
 const styleFields = computed(() => getMaterialFields(selectedNode.value!.type))
+
+const activeTab = ref('style')
 </script>
 
 <template>
-  <Tabs default-value="style" class="min-h-0 flex-1 gap-0">
+  <Tabs v-model="activeTab" class="min-h-0 flex-1 gap-0">
     <div class="border-b px-3 py-2">
       <TabsList class="grid w-full grid-cols-3">
         <TabsTrigger value="layout">
